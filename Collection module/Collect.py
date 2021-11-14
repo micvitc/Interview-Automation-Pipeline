9b1757296ce97393ea0868da0ae0493d8b3ee18d
Aravind <70009354+ARTHURFLECK1828@users.noreply.github.com>
2021-11-14 19:20:46 +0530

    Updated to a better library
    Due to shareplum being out of order , alternatively office365 library is put into use

--- Collection module/Collect.py
+++ Collection module/Collect.py
@@ -1,36 +1,34 @@
-#discord id- mememan#2565
-
-from shareplum import Site
-from shareplum import Office365
-from time import sleep
-import json
-
-def getdata():
-
-    """
-    function that scrapes data & saves it into a JSON file the below line is the authorization cookie the <link> is
-    the main sharepoint link of the organization in our case it would be https://vitacin-my.sharepoint.com/ 
-    username is email id & password is your microsoft acc password
-    """    
-
-    authcookie = Office365('https://vitacin-my.sharepoint.com/', username='', password='').GetCookies()
-
-    """
-    below the link is to your personal folder inside sharepoint 
-    in my case it was https://vitacin-my.sharepoint.com/personal/aravind_p2020_vitstudent_ac_in/
-    change yours accordingly
-    """
-
-    site = Site('https://vitacin-my.sharepoint.com/personal/aravind_p2020_vitstudent_ac_in/', authcookie=authcookie)
-    sp_list = site.List('<list name>')  #<list name> - name of the list 
-    data = sp_list.GetListItems('All Items')    # gives output as a list of dictionaries
-
-if __name__=="__main__" :
-    while True:
-        getdata()
-        sleep(60 * 60)  #automatically fetches data every hour 
+from office365.runtime.auth.user_credential import UserCredential
+from office365.sharepoint.client_context import ClientContext
 
 """
-Sample Output : [{'Title': 'me', 'ch1': 'cs', 'ch2': 'valo', 'phno': '1'}]
-shareplum docs https://pypi.org/project/SharePlum/
+Code snippet below is to generate a query and return the list
 """
+site_url = "<url of sharepoint>"
+sp_list = "<list-name>"
+ctx = ClientContext(site_url).with_credentials(UserCredential("<mailid>", "<password>"))
+sp_lists = ctx.web.lists
+s_list = sp_lists.get_by_title(sp_list)
+l_items = s_list.get_items()
+ctx.load(l_items)
+ctx.execute_query()
+
+'''
+Code snippet below is to remove junk entries 
+'''
+
+ls=['FileSystemObjectType','ServerRedirectedEmbedUri','ServerRedirectedEmbedUrl','ID','ContentTypeId','Modified','Created','AuthorId','EditorId','OData__UIVersionString','Attachments','GUID', 'ComplianceAssetId']
+l=[]
+DATA=[]
+for item in l_items:
+    l.append(item.properties)
+for dic in l:
+    DATADIC={}
+    for ele in dic:
+        if ele not in ls:
+            dat=dic.get(ele)
+            DATADIC[ele]=dat
+    DATA.append(DATADIC)
+
+# OUTPUT
+print(DATA)     #List of dictionaries
